a2c2bda9ad382e91776b15fcfb4e7819f1efffc6
ayrat <ajratb@ya.ru>
2023-08-13 23:13:36 +0700

    Complete project v.1.0.0

--- lqbase-clickhouse-gradle/app/src/main/java/com/example/ClickHouseConnection.java
+++ lqbase-clickhouse-gradle/app/src/main/java/com/example/ClickHouseConnection.java
@@ -1,12 +1,22 @@
 package com.example;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.Scope;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClickHouseConnection {
 
-    private final String url = "jdbc:clickhouse://localhost/tutorial";
-
     public Connection connect() {
+        String url = "jdbc:clickhouse://localhost:8123/tutorial";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, "", "");
@@ -18,6 +28,29 @@ public class ClickHouseConnection {
         return conn;
     }
 
+    public void liquibase() throws Exception{
+        Map<String, Object> config = new HashMap<>();
+//        config.put("liquibase.url", "jdbc:clickhouse://localhost:8123/default");
+        config.put("liquibase.url", "jdbc:clickhouse://localhost:8123");
+        config.put("driver", "com.clickhouse.jdbc.ClickHouseDriver");
+        config.put("databaseClass", "liquibase.ext.clickhouse.database.ClickHouseDatabase");
+        config.put("strict", "true");
+//        config.put("defaultSchemaName", "default");
+//        config.put("liquibaseSchemaName", "default");
+
+        Scope.child(config, () -> {
+            Connection connection = connect(); //your openConnection logic
+
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+//            database.setLiquibaseSchemaName("default");
+            database.setDefaultSchemaName("DUMMY");
+//            database.setDatabaseChangeLogTableName("NEWCHANGE");
+            Liquibase liquibase = new liquibase.Liquibase("db/clickhouse/00-registry.yaml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update(new Contexts(), new LabelExpression());
+            //Liquibase calls will go here
+        });
+    }
+
     public static void main(String[] args) throws Exception {
         ClickHouseConnection app = new ClickHouseConnection();
         Connection connect = app.connect();
@@ -25,7 +58,8 @@ public class ClickHouseConnection {
         ResultSet resultSet = st.executeQuery();
         boolean next = resultSet.next();
         String string = resultSet.getString(3);
-        System.out.println("end");
+        System.out.println("end: " + string);
         connect.close();
+        app.liquibase();
     }
 }
---
+++ lqbase-clickhouse-gradle/app/src/main/resources/db/clickhouse/02-table-dummytable.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+
+--changeset johndoe:create-dummy-table
+
+CREATE TABLE dummy.dummytable
+(
+
+    `WatchID` UInt64,
+
+    `JavaEnable` UInt8,
+
+    `Title` String
+)
+ENGINE = MergeTree
+ORDER BY WatchID
+SETTINGS index_granularity = 8192;
